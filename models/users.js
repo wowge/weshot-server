1e79b44590b95c6dac2f8eb23b5484c818042c06
wowge <you@example.com>
2017-09-19 16:44:42 +0800

    passport.use() +6

--- models/users.js
+++ models/users.js
@@ -17,7 +17,6 @@ userSchema.methods.validPassword = function (password) {
 userSchema.methods.generateJwt = function () {
   var expiry = new Date()
   expiry.setDate(expiry.getDate() + 7)
-  console.log(process.env.NODE_ENV)
   return jwt.sign({
     _id: this._id,
     exp: parseInt(expiry.getTime()/1000)
